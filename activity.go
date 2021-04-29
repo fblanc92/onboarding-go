e397b7b028d40a0df38addd03d48f662004b8b94
Juan Bolaño <juan.bolagno@gmail.com>
2021-04-29 18:58:57 -0300

    Clean activity

--- activity.go
+++ activity.go
@@ -5,30 +5,6 @@ import (
 	"fmt"
 )
 
-// TRANSFER MONEY
-// func Withdraw(ctx context.Context, transferDetails TransferDetails) error {
-// 	fmt.Printf(
-// 		"\nWithdrawing $%f from account %s. ReferenceId: %s\n",
-// 		transferDetails.Amount,
-// 		transferDetails.FromAccount,
-// 		transferDetails.ReferenceID,
-// 	)
-// 	return nil
-// }
-
-// @@@SNIPSTART onboarding-go-activity
-// func Deposit(ctx context.Context, transferDetails TransferDetails) error {
-// 	fmt.Printf(
-// 		"\nDepositing $%f into account %s. ReferenceId: %s\n",
-// 		transferDetails.Amount,
-// 		transferDetails.ToAccount,
-// 		transferDetails.ReferenceID,
-// 	)
-// 	// Switch out comments on the return statements to simulate an error
-// 	//return fmt.Errorf("deposit did not occur due to an issue")
-// 	return nil
-// }
-
 //ONBOARDING
 func GotoNextDivision(ctx context.Context, onboardingDetails OnBoardingTask) error {
 	fmt.Printf(
