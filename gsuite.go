6b23892d13eaf946f522106bb066b5b9c82b16d6
Federico Blanc <federico.blanc@electric.ai>
2021-04-30 00:35:32 -0300

    Errors in activities implemented

--- gsuite.go
+++ gsuite.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"log"
 	"time"
 
 	"go.temporal.io/sdk/workflow"
@@ -17,6 +18,11 @@ func GsuiteWorkflow(ctx workflow.Context, name string) (string, error) {
 	invocations := [10]string{"SIGN IN admin.google.com", "Add user data", "Update Org Units", "Fill Wellcome Template", "Create New Password", "Finish User Creation", "Close Ticket", "Finish Welcome Template", "Add User To Groups", "Add User To Calendars With Correct Permissions"}
 	var result, result_gsuite string
 	for i := 0; i < 10; i++ {
+		if invocations[i] == "Create New Password" {
+			err := "UNABLE TO CONTINUE in ---" + invocations[i]
+			log.Fatalln(err)
+			return err, nil
+		}
 		err := workflow.ExecuteActivity(ctx, GsuiteActivity, invocations[i]).Get(ctx, &result)
 		result_gsuite += result + time.Now().String() + "\n"
 		if err != nil {
